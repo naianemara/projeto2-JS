5bc3af72a823fc6b3716a1cc18b68e8205d3407e
naianemara <maranaiane112@gmail.com>
2024-01-02 18:37:22 -0300

    adicão de "if" e "else"

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-entrada").value)
     const max = Math.floor(document.querySelector(".input-saida").value)
 
-    const result = Math.floor(Math.random() * (max - min) + min);
 
-    alert(result)
+    if (max > min) {
+        const result = Math.floor(Math.random() * (max - min) + min);
+
+        alert(result)
+    }
+
+    else {
+        alert ("O valor minino tem que ser Menor que o valor maxino")
+    }
+
+
 
 }
